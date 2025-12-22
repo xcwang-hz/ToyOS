927335205797ab6a1107524dbeaf8de6f7210a83
Xiaochu Wang <xiaochu.wang@gmail.com>
2025-12-22 12:17:10 -0600

    ./config.sh -d will generate debug build

--- config.sh
+++ config.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+BUILD_TYPE="Release"
+
+# If -d is passed, switch to Debug mode
+while getopts "d" opt; do
+  case $opt in
+    d)
+      echo ">> [INFO] Debug mode enabled (-g -O0)"
+      BUILD_TYPE="Debug"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Create build directory
 mkdir -p build
 cd build
-CC=clang CXX=clang++ cmake ..
+
+# Run CMake with the specified build type
+echo ">> Configuring project with type: $BUILD_TYPE"
+CC=clang CXX=clang++ cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE
